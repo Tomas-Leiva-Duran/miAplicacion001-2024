ff3017b4bd1fd1a9752c34dc0becf786751b0bdb
Tomas-Leiva-Duran <to.leiva@duocuc.cl>
2024-01-05 20:37:29 -0300

    cambio

---
+++ src/app/Inicio/Inicio.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Inicio',
+  templateUrl: 'Inicio.page.html',
+  styleUrls: ['Inicio.page.scss']
+})
+export class InicioPage {
+
+  constructor() {}
+
+}
